2fc8eeaea5614ceb43a3b69da5d3f38505cde362
ozankaze <ozankaze101@gmail.com>
2018-07-24 08:11:11 +0700

    metode set dan metode post

--- index.php
+++ index.php
@@ -5,25 +5,45 @@
 class Kucing {
 
     // properti
-    public $suara = 'meong meong';
-    public $berat = 30;
+    public $suara;
+    public $berat;
 
-    // metode
-    public function bersuara() 
+    // metode set && get
+    public function set_suara($suara) 
     {
-        echo 'suara Kucing' ." ...... ". $this->suara;
-        // $this guna untuk mengambil peroperti yang ada di dalam dirinya sendiri
+        $this->suara = $suara;
     }
 
-    public function berat_kucing()
+    public function get_suara()
+    {
+        return $this->suara;
+    }
+
+    public function set_berat_kucing($kucing)
+    {
+        $this->berat = $kucing;
+    }
+
+    public function get_kucing()
     {
         return $this->berat;
     }
 }
 
 $kucing1 = new Kucing;
-$kucing1->bersuara();
-echo $kucing1->berat_kucing();
+$kucing2 = new Kucing;
+
+$kucing1->set_suara('meong meong wkwk');
+echo 'bunyinya .... ' . $kucing1->get_suara();
+echo "<br>";
+
+$kucing2->set_suara('Hmmmmmm Meong');
+echo 'bunyinya .... ' . $kucing2->get_suara();
+echo "<br>";
+
+// $kucing1->set_berat_kucing(40);
+// echo 'beratnya' . $kucing1->get_kucing();
+// echo "<br>";
 // var_dump($kucing1);
 
 ?>
