54ffac58c627e9bda9c1c4b0c422dc849257b4f6
ozankaze <ozankaze101@gmail.com>
2018-07-30 14:08:45 +0700

    ruang lingkup public, protected, private

--- Kucing.php
+++ Kucing.php
@@ -4,9 +4,13 @@
 
 class Kucing { // anak
 
+    // darimana ajah properti dapat di akses
+
     // properti
-    public $suara;
+    protected $suara; // hanya bisa di akses oleh kelas yang mewarisi, ketika kita membuat pro kita tidak bisa lagi mengakses langsung propertinya, tapi properti bisa di akses oleh kelas itu sendiri / kelas yang mewarisinya
     public $berat;
+    // public bisa di akses di mana sajah
+    // private  yanya bisa di akses oleh kelas itu sendiri
 
     // konstrucktor metode fungsi biasa akan otomatis terpanggil ketika buat objecknya
     // setiap membuat object dia akan menmanggil apapun yang ada di dalamnya
--- index.php
+++ index.php
@@ -2,16 +2,8 @@
 require_once('Kucing.php');
 require_once('IbuKucing.php');
 
-$kucing1 = new Kucing('meong meong wkwk', 20);
-// $kucing2->set_berat_kucing(40);
-// echo 'beratnya .... ' . $kucing2->get_kucing() . "<br>";
-echo 'Anaknya bunyinya .... ' . $kucing1->get_suara() . "<br>";
-
-
-$kucingAngora = new IbuKucing('hoam hoam', 20);
-echo $kucingAngora->get_suara() . "<br>";
-
-// echo 'Ibunya angora ...... ' . $kucingAngora->get_imut() . "<br>";
+$kucing = new IbuKucing('ngik ngik', 20);
+echo $kucing->get_suara();
 
 
 ?>
