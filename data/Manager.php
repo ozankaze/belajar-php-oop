133e2d98e3f79f9ba2ee1457428632eb41061512
ozankaze <ozankaze101@gmail.com>
2023-03-12 14:11:14 +0700

    PHP Object Oriented Programming

--- data/Manager.php
+++ data/Manager.php
@@ -3,6 +3,13 @@
 class Manager
 {
     var string $name;
+    var string $title;
+    
+    public function __construct(string $name = "", string $title = "Manager")
+    {
+        $this->name = $name;
+        $this->title = $title;        
+    }
 
     function sayHello(string $name) : void 
     {
@@ -12,6 +19,12 @@ class Manager
 
 class VicePresident extends Manager
 {
+    public function __construct(string $name = "")
+    {
+        // tidak wajib, tapi di rekomendasikan
+        parent::__construct($name, "VP");
+    }
+
     function sayHello(string $name) : void 
     {
         echo "Hi $name, my name is $this->name sebagai VicePresident" . PHP_EOL;
