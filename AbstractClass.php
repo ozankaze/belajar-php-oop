9e5d3fb547c7deb65f771f68983b6fe594687d59
ozankaze <ozankaze101@gmail.com>
2023-03-16 21:23:14 +0700

    PHP Object Oriented Programming Abstract Class

---
+++ AbstractClass.php
@@ -0,0 +1,12 @@
+<?php
+
+require "data/Location.php";
+
+use Data\{Location, City, Province, Country};
+
+// $location = new Location(); ERROR
+
+$city = new City();
+$province = new Province();
+$country = new Country();
+
