b4a8dba0c342798642b7978565a0fc14c28e4dc8
ozankaze <ozankaze101@gmail.com>
2023-03-16 21:03:44 +0700

    PHP Object Oriented Programming Type Check & Casts

--- data/Programmer.php
+++ data/Programmer.php
@@ -28,5 +28,11 @@ class Company
 
 function sayHello(Programmer $programmerku)
 {
-    echo "Hello programmer $programmerku->name" . PHP_EOL;
+    if ($programmerku instanceof BackendProgrammer) {
+        echo "Hello backend programmer $programmerku->name" . PHP_EOL;
+    } elseif ($programmerku instanceof FrontendProgrammer) {
+        echo "Hello fronend programmer $programmerku->name" . PHP_EOL;
+    } elseif ($programmerku instanceof Programmer) {
+        echo "Hello programmer $programmerku->name" . PHP_EOL;
+    }
 }
