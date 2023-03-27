4ffee62d1d2c6314b31697f6a0d6944ea0c45342
ozankaze <ozankaze101@gmail.com>
2023-03-27 17:45:42 +0700

    PHP Object Oriented Programming Final Function

--- data/SocialMedia.php
+++ data/SocialMedia.php
@@ -5,13 +5,21 @@ class SocialMedia
     public string $name;
 }
 
-final class Instagram extends SocialMedia
+// final +
+class Instagram extends SocialMedia
 {
-
+    // final +
+    public function login(string $username, string $password)
+    {
+        return true;
+    }
 }
 
 // error
-// class Faceook extends Instagram
-// {
- 
-// }
+class Faceook extends Instagram
+{
+    public function login(string $username, string $password)
+    {
+        return false;
+    }
+}
